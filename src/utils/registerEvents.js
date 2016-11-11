d5c6d32fcf4fe46e944052fdf19ae42567702f72
atomita <sleeping.cait.sith+gh@gmail.com>
2016-11-11 11:43:15 +0900

    If riot.js v2.6 then Since "handlers" may not inherit Object, handlers.hasOwnProperty is not used

--- src/utils/registerEvents.js
+++ src/utils/registerEvents.js
@@ -1,12 +1,20 @@
 
-let addListener = function() {};
+let hasOwnProperty = function(obj, prop){
+      try {
+        return Object.prototype.hasOwnProperty.call(obj, prop);
+      }
+      catch (e) {
+        return (prop in obj);
+      }
+    },
+    addListener = function() {};
 if(!!window.google) { addListener = google.maps.event.addListener };
 
 export default function registerEvents(eventList, handlers, instance) {
   const registeredEvents = eventList.reduce((acc, eventName) => {
     const onEventName = `on${eventName}`;
 
-    if (handlers.hasOwnProperty(onEventName)) {
+    if (hasOwnProperty(handlers, onEventName)) {
       acc.push(addListener(instance, eventName, handlers[onEventName]));
     }
 
